3fedd13868e5d9494ffa8296082a94b227ffa2bb
GeorgeLnA <d@example.com>
2025-12-29 21:51:25 +0000

    Fix Sharp logo size to be consistent across all screen sizes
    - Changed from responsive sizing (h-[27px] sm:h-[31px] md:h-[35px] lg:h-[43px]) to fixed size (h-[35px])
    - Logo now displays at the same size on all devices

--- src/components/ui/home-smooth-scroll.tsx
+++ src/components/ui/home-smooth-scroll.tsx
@@ -96,7 +96,7 @@ const HomeSmoothScroll = forwardRef<HTMLElement>((props, ref) => {
               <img 
                 src="/sharp_logo_high_res.png" 
                 alt="Sharp Logo" 
-                className="h-[27px] sm:h-[31px] md:h-[35px] lg:h-[43px] w-auto object-contain opacity-100"
+                className="h-[35px] w-auto object-contain opacity-100"
               />
             </div>
           </section>
